bf8ba76392e106ecdcf1b27738509955203b4d5a
Shanna Sullivan <shanna.sullivan@gmail.com>
2015-12-17 16:54:27 -0800

    starting to incorporate game into game view by adding custom directive. added function that destroys game scope when leaving that view

--- client/app/game/game.js
+++ client/app/game/game.js
@@ -7,4 +7,31 @@ angular.module('dreamDash.game', [
 // TODO: make sure this is minification safe
 .controller('GameController', function($scope) {
 	console.log("inside GameController");
+
+	// TODO: clean up game/destroy game when user leaves game view
+	$scope.$on('$destroy', function() {
+		console.log("destroying game");
+		// uncomment this when game is added
+		// game.destroy();
+	})
 })
+
+// this is the directive to run the game
+// best practice to add custom prefix to directives to avoid future collisions
+.directive('myGameCanvas', function($injector) {
+	console.log("inside myGameCanvas directive")
+	// when directive is attached to page, its link function will get called
+	// add a link function that creates an isolated scope
+	var linkFunction = function(scope, element, attributes) {
+		createGame(scope, scope.mapId, $injector);
+	};
+	return {
+		scope: {
+			// not sure if mapId is actually needed yet
+			mapId: '='
+		},
+		templateUrl: 'gameView.html',
+		link: linkFunction
+	}
+})
+
