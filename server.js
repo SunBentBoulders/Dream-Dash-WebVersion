d7ef10195d868c5c9b601675979bfbe56e32ee8f
Jazz Lyles <ripley36706@gmail.com>
2015-12-18 15:44:42 -0800

    static test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public_html'));
+app.use(express.static(__dirname + './public_html'));
 
 app.listen(2000);
 app.listen(app.get('port'), function(){
