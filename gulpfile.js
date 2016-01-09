25bad9c77614420623496fe89fd714252595f23f
Darryl Nunn <darrylnu@gmail.com>
2016-01-08 16:55:40 -0800

    updated files for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -21,12 +21,12 @@ var git = require('gulp-git');
 
 var paths = {
     scripts: [
-        'public_html/app.js',
-        'public_html/factories/**/*.js',
-        'public_html/aboutGame/**/*.js',
-        'public_html/info/**/*.js',
-        'public_html/game/**/*.js',
         'public_html/vendor/**/*.js',
+        'public_html/app.js',
+        'public_html/aboutGame/**/*.js',
+        'public_html/game/**/*.js',
+        'public_html/info/**/*.js',
+        'public_html/factories/**/*.js',
         'public_html/lib/**/*.js',
         'public_html/src/**/*.js'
     ],
@@ -136,28 +136,28 @@ gulp.task('cleanup', function(){
 
 //Start of Git TASKS
 
-gulp.task('clone', function(){
-  git.clone('https://github.com/sunbentboulders/Dream-Dash-WebVersion.git', function (err) {
-    if (err) throw err;
-  });
+gulp.task('clone', function(){
+  git.clone('https://github.com/sunbentboulders/Dream-Dash-WebVersion.git', function (err) {
+    if (err) throw err;
+  });
 });
 
-gulp.task('checkout', function(){
-  git.checkout('branchName', function (err) {
-    if (err) throw err;
-  });
+gulp.task('checkout', function(){
+  git.checkout('branchName', function (err) {
+    if (err) throw err;
+  });
 });
 
-gulp.task('pull', function(){
-    git.pull('upstream', ' ', {args: '--rebase'}, function (err) {
-        if (err) throw err;
+gulp.task('pull', function(){
+    git.pull('upstream', ' ', {args: '--rebase'}, function (err) {
+        if (err) throw err;
     });
 });
 
-gulp.task('push', function(){
-  git.push('origin', 'master', {args: " -f"}, function (err) {
-    if (err) throw err;
-  });
+gulp.task('push', function(){
+  git.push('origin', 'master', {args: " -f"}, function (err) {
+    if (err) throw err;
+  });
 });
 
 gulp.task('default', [
